fe0608c0b9979ead0ba6ec47b15114bb27294f66
Ali M <alimousavi-01@outlook.com>
2022-07-13 14:49:50 +0430

    Added MongoDb Connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("Successfully connected to Database"))
+.catch(err => console.log(err));
 
 app.listen(8080, () => {
     console.log("Running on https://localhost://8080");
