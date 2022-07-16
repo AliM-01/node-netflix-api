cb17442d4944b64358011d65d7e89aa877529c1e
Ali M <alimousavi-01@outlook.com>
2022-07-16 19:56:07 +0430

    users get all/ get by id routes

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -4,6 +4,45 @@ import { UserModel } from '@models';
 
 const router = express.Router();
 
+router.get("/", verify, async (req: Request, res: Response) => {
+    
+    if (!req.user.isAdmin)
+        res.status(403);
+
+    const query = req.query.new;
+
+    try {
+        let users;
+
+        if (query)
+            users = await UserModel.find().limit(10)
+        else
+            users = await UserModel.find();
+
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get("/:id", verify, async (req: Request, res: Response) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+        try {
+            const user = await UserModel.findById(req.user.id);
+
+            res.status(200).json({
+                "uid": user?._id,
+                username: user?.username,
+                email: user?.email,
+            });
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403);
+    }
+});
+
 // PUT /api/users/:id
 router.put("/:id", verify, async (req: Request, res: Response) => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
@@ -49,7 +88,7 @@ router.delete("/:id", verify, async (req: Request, res: Response) => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
         try {
             await UserModel.findByIdAndDelete(req.user.id);
-            res.status(200).json({message: "User has been deleted !"});
+            res.status(200).json({ message: "User has been deleted !" });
         } catch (err) {
             res.status(500).json(err);
         }
